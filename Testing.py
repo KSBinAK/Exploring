7940e829a95d0b8fb5621483f8d6c89722cb4cf3
KSBinAK <87298192+KSBinAK@users.noreply.github.com>
2021-07-13 14:09:55 -0800

    Add files via upload

---
+++ Testing.py
@@ -0,0 +1,16 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello, World!")
+Hello, World!
+>>> print(3+4)
+7
+>>> print("3+4")
+3+4
+>>> Print("How do we save this?")
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    Print("How do we save this?")
+NameError: name 'Print' is not defined
+>>> print("Oops!  Can't use capital letters on functions!")
+Oops!  Can't use capital letters on functions!
+>>> 
